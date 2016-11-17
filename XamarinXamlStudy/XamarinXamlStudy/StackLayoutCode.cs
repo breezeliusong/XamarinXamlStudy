eaf48b87f3362ca941e22b0eb92b61949da6c475
liusong <liusong@V-DOXIE-W10>
2016-11-17 14:38:12 +0800

    Add project files.

---
+++ XamarinXamlStudy/XamarinXamlStudy/StackLayoutCode.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinXamlStudy
+{
+    public class StackLayoutCode:ContentPage
+    {
+        public StackLayoutCode()
+        {
+            var layout = new StackLayout();
+            var button = new Button { Text = "StackLayout", VerticalOptions = LayoutOptions.Start, HorizontalOptions = LayoutOptions.FillAndExpand };
+            button.Clicked += Button_Clicked;
+            var redBox = new BoxView { Color = Color.Red, VerticalOptions = LayoutOptions.FillAndExpand, HorizontalOptions = LayoutOptions.FillAndExpand };
+            layout.Children.Add(button);
+            layout.Children.Add(redBox);
+            layout.Spacing = 10;
+            Content = layout;
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new StackLayoutPage());
+        }
+
+    }
+}
---
+++ XamarinXamlStudy/XamarinXamlStudy/StackLayoutPage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace XamarinXamlStudy
+{
+    public class StackLayoutPage : ContentPage
+    {
+        public StackLayoutPage()
+        {
+            //Content = new StackLayout
+            //{
+            //    Children = {
+            //        new Label { Text = "Hello Page" }
+            //    }
+            //};
+            var layout = new StackLayout();
+            var button = new Button { Text = "StackLayout", VerticalOptions = LayoutOptions.Start, HorizontalOptions = LayoutOptions.FillAndExpand };
+            button.Clicked += Button_Clicked;
+            var greenBox = new BoxView { Color = Color.Green, VerticalOptions = LayoutOptions.FillAndExpand, HorizontalOptions = LayoutOptions.FillAndExpand };
+            layout.Children.Add(button);
+            layout.Children.Add(greenBox);
+            layout.Spacing = 10;
+            Content = layout;
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new BindingPage());
+        }
+    }
+}
